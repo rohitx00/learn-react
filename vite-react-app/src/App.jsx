d952f987f16eb9beb5868d6d20b58795479d71ed
rohitx00 <rohitkumhar848@gmail.com>
2025-01-12 02:19:55 +0530

    Learned about Tailwind css and props

--- vite-react-app/src/App.jsx
+++ vite-react-app/src/App.jsx
@@ -1,9 +1,18 @@
-import Test from './test'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Cards";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <Test />
-  )
+    <>
+      <Card price="120" jacketName="Winter Jacket" />
+      <Card price="150" jacketName="Classic black leather Jacket" />
+    </>
+  );
 }
 
 export default App;
